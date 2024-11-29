601c44069965b8fc31c015aa81601813f60c08bd
georgeK <george.kokolakis@learnworlds.com>
2024-11-29 18:15:29 +0200

    Removed Commented Out Lines

--- TaskManagementSystem/src/main/java/com/medialab/MainApp.java
+++ TaskManagementSystem/src/main/java/com/medialab/MainApp.java
@@ -11,26 +11,24 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
     private TaskManager taskManager;
-    // private MainController mainController;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/medialab/views/MainLayout.fxml"));
-        Parent root = loader.load();
-
         // Initialize singleton
         taskManager = TaskManager.getInstance();
-        taskManager.load(); // Load tasks from file
+        // Load tasks from file
+        taskManager.load(); 
         // Check for delayed tasks and update their status
         taskManager.checkDelayedTasks();
-
-        // Show a pop-up window with delayed tasks
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/medialab/views/MainLayout.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("MediaLab Assistant");
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setMinWidth(1200);
         primaryStage.setMinHeight(800);
-
+        // Show a pop-up window with delayed tasks
         taskManager.showDelayedTasksPopup();
         taskManager.initializeReminderChecker();
     }
